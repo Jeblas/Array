55d4363dc691209db48778e2b59cc0ddcfb11d63
John Blasco <jeblas@Lynx.local>
2018-10-10 14:44:12 -0400

    Updated Simple String to contain move constructor - From piazza

--- src/simple_string.h
+++ src/simple_string.h
@@ -12,11 +12,12 @@ using u32 = uint32_t;
 class simple_string {
 public:
     simple_string();
-    simple_string(const char* str);
+    explicit simple_string(const char* str);
     simple_string(const simple_string& cpy);
-    simple_string(simple_string&& mv);
+    simple_string(simple_string&& mv) noexcept;
     ~simple_string();
     simple_string& operator=(const simple_string&);
+    simple_string& operator=(simple_string&&) noexcept;
     const char* c_str() const;
 
     static void initialize_counts();
@@ -30,10 +31,9 @@ private:
     static u32 copy_count;
     static u32 assign_count;
     static u32 destruct_count;
-    static u32 move_const_count;
-    static u32 move_assign_count;
+    static u32 move_count;
+    static u32 move_assign;
 
 };
 
 std::ostream& operator<<(std::ostream&, const simple_string& st);
-
