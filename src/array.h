71474618f3ac85d26e7d60286a32fc71cfad269b
John Blasco <jeblas@Lynx.local>
2018-10-12 16:00:34 -0400

    tabs -> 8 spaces

--- src/array.h
+++ src/array.h
@@ -107,17 +107,17 @@ public:
             m_reserved_size *= 2;
             m_elements = (T*)malloc(sizeof(T) * m_reserved_size);
 
-	    for (std::size_t i = m_size; i>0; --i) {
-	        new (m_elements + i) T(std::move(old_elements[i - 1]));
+            for (std::size_t i = m_size; i>0; --i) {
+                new (m_elements + i) T(std::move(old_elements[i - 1]));
                 m_elements[i - 1].~T();
-	    }
+            }
             free(old_elements);
         } else {
-	    for (std::size_t i = m_size; i>0; --i) {
-	        new (m_elements + i) T(std::move(m_elements[i - 1]));
+            for (std::size_t i = m_size; i>0; --i) {
+                new (m_elements + i) T(std::move(m_elements[i - 1]));
                 m_elements[i - 1].~T();
-	    }
-	}
+            }
+        }
         new (m_elements) T(t);
         ++m_size;
     }
@@ -213,30 +213,30 @@ public:
     //insert element right before itr
     void insert(const T& t, const array_iterator<T>& ai) {
         // move elements right by 1 and insert value
-	array_iterator<T> it(ai);
+        array_iterator<T> it(ai);
         if (m_size == m_reserved_size) {
             T* old_elements = m_elements;
             m_reserved_size *= 2;
             m_elements = (T*)malloc(sizeof(T) * m_reserved_size);
-	    
-	    std::size_t count= 0;
+            
+            std::size_t count= 0;
             for (array_iterator<T> it(ai); it != ai; ++it) {
-	        //move each element from the old_element to
-	//	new (m_elements)
-	    }
+                //move each element from the old_element to
+        //        new (m_elements)
+            }
 
 
-	    for (std::size_t i = m_size; i>0; --i) {
-	        new (m_elements + i) T(std::move(old_elements[i - 1]));
+            for (std::size_t i = m_size; i>0; --i) {
+                new (m_elements + i) T(std::move(old_elements[i - 1]));
                 m_elements[i - 1].~T();
-	    }
+            }
             free(old_elements);
         } else {
-	    for (std::size_t i = m_size; i>0; --i) {
-	        new (m_elements + i) T(std::move(m_elements[i - 1]));
+            for (std::size_t i = m_size; i>0; --i) {
+                new (m_elements + i) T(std::move(m_elements[i - 1]));
                 m_elements[i - 1].~T();
-	    }
-	}
+            }
+        }
         new (m_elements) T(t);
         ++m_size;
     }
